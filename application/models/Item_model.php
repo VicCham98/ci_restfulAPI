c16077054638264b155fdb4090106857954b15ed
VicCham98 <victor_cha_q@hotmail.com>
2020-03-13 01:24:13 -0500

    Update Item_model.php
    use join/from/where/select

--- application/models/Item_model.php
+++ application/models/Item_model.php
@@ -4,6 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Item_model extends CI_Model {
 
+	public function getProductos(){
+		$this->db->select("p.*,c.nombre as categoria");
+		$this->db->from("productos p");
+		$this->db->join("categorias c", "p.categoria_id = c.id");
+		$this->db->where("p.estado","1");
+		$resultados = $this->db->get();
+		return $resultados->result();
+	}
+
 	public function index_get($id){
 		if(!empty($id)){
 			$this->db->where('id', $id);
